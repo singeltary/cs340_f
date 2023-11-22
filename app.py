5a40be4bee6a3a2f718e864bec942703145889c6
singeltary <31216523+singeltary@users.noreply.github.com>
2023-11-22 13:46:52 -0500

    Update app.py
    Read Donors should work now, started update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, request, redirect
 import os
 import database.db_connector as db
 
@@ -20,7 +20,9 @@ def display_donors():
         cursor = db.execute_query(db_connection=db_connection, query=query)
         results = cursor.fetchall()
         return render_template("donors.j2", people=results)
-        
+ 
+@app.route('/add_donor', methods=['POST', 'GET'])
+def add_donor(): 
     if request.method == "POST":
         name = request.form.get("name")
         street = request.form.get("street")
@@ -32,6 +34,26 @@ def display_donors():
         query = "INSERT INTO Donors (name, street, city, state_ab, zip, bloodType) VALUES (%s, %s, %s, %s, %s, %s);"
         cur = db.execute_query(db_connection=db_connection, query=query, query_params=(name, street, city, state_ab, zip, bloodType))
         return redirect("/donors")
+      
+@app.route('/update_donor/<int:donorID>', methods=['GET', 'POST'])
+def update_donor(donorID):
+    if request.method == "GET":
+        query1 = "SELECT * FROM Donors WHERE donorID = %s" % donorID
+        cur_get = db.execute_query(db_connection=db_connection, query=query1)
+        donor_up = cur_get.fetchall()
+        return render_template("update_donor.j2", donor=donor_up)
+    if request.method == "POST":
+        name = request.form.get("name")
+        street = request.form.get("street")
+        city = request.form.get("city")
+        state_ab = request.form.get("state")
+        zip = request.form.get("zip")
+        bloodType = request.form.get("type")
+        
+        query2 = "UPDATE Donors SET Donors.name = %s, Donors.street = %s, Donors.city = %s, Donors.state_ab = %s, Donors.zip = %s, Donors.bloodType = %s WHERE Donors.donorID = %s"
+        cur_post = db.execute_query(db_connection=db_connection, query=query2, query_params=(name, street, city, state_ab, zip, bloodType))
+        return redirect("/donors")
+
 
 # Listener
 
