e92d57832b4aa1c972964eb7a8d1e6ace165d48d
singeltary <31216523+singeltary@users.noreply.github.com>
2023-11-30 14:49:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
-from flask import Flask, render_template, json, request, redirect, url_for
-import os
-import database.db_connector as db
+from flask import Flask, render_template, json, redirect
 from flask_mysqldb import MySQL
-
-# Configuration
+from flask import request
+import os
 
 app = Flask(__name__)
-db_connection = db.connect_to_database()
 
-# Routes 
+app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
+app.config['MYSQL_USER'] = 'cs340_singeltb'
+app.config['MYSQL_PASSWORD'] = '0566' #last 4 of onid
+app.config['MYSQL_DB'] = 'cs340_singeltb'
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
+
+mysql = MySQL(app)
+
+
+# Routes
 @app.route('/')
 def root():
     return render_template("main.j2")
-
-#Read Donors
-@app.route('/donors', methods=['POST', 'GET'])
-def display_donors():
-    if request.method == "GET":
-        query = "SELECT * FROM Donors;"
-        cursor = db.execute_query(db_connection=db_connection, query=query)
-        results = cursor.fetchall()
-        return render_template("donors.j2", people=results)
- 
-#Create new donor
-@app.route('/add_donor', methods=['POST', 'GET'])
+    
+@app.route('/donors', methods = ["GET", "POST"])
+def donors():
+    query1 = 'SELECT * FROM Donors;';
+    cur = mysql.connection.cursor()
+    cur.execute(query1)
+    results = cur.fetchall()
+    return render_template("donors.j2", people=results)
+    
+@app.route('/add_donor', methods=["POST", "GET"])
 def add_donor(): 
     if request.method == "POST":
         name = request.form.get("name")
@@ -34,18 +38,20 @@ def add_donor():
         zip = request.form.get("zip")
         bloodType = request.form.get("type")
                 
-        query = "INSERT INTO Donors (name, street, city, state_ab, zip, bloodType) VALUES (%s, %s, %s, %s, %s, %s);"
-        cur = db.execute_query(db_connection=db_connection, query=query, query_params=(name, street, city, state_ab, zip, bloodType))
+        query_add = "INSERT INTO Donors (name, street, city, state_ab, zip, bloodType) VALUES (%s, %s, %s, %s, %s, %s);"
+        cur = mysql.connection.cursor()
+        cur.execute(query_add, (name, street, city, state_ab, zip, bloodType))
+        mysql.connection.commit()
         return redirect("/donors")
-
-#Update donor    
-@app.route('/update_donor/<int:donorID>', methods=['GET', 'POST'])
+        
+@app.route('/update_donor/<int:donorID>', methods=["GET", "POST"])
 def update_donor(donorID):
     if request.method == "GET":
         query1 = "SELECT * FROM Donors WHERE donorID = %s" % donorID
-        cur_get = db.execute_query(db_connection=db_connection, query=query1)
-        donor_up = cur_get.fetchall()
-        return render_template("update_donor.j2", donor=donor_up)
+        cur = mysql.connection.cursor()
+        cur.execute(query1)
+        results = cur.fetchall()
+        return render_template("update_donor.j2", donor=results)
     if request.method == "POST":
         name = request.form.get("name")
         street = request.form.get("street")
@@ -54,18 +60,20 @@ def update_donor(donorID):
         zip = request.form.get("zip")
         bloodType = request.form.get("type")
         query2 = "UPDATE Donors SET Donors.name = %s, Donors.street = %s, Donors.city = %s, Donors.state_ab = %s, Donors.zip = %s, Donors.bloodType = %s WHERE Donors.donorID = %s"
-        cur_post = db.execute_query(db_connection=db_connection, query=query2, query_params=(name, street, city, state_ab, zip, bloodType, donorID))
-        return render_template('success.j2')
-
-#Delete donor
+        
+        cur = mysql.connection.cursor()
+        cur.execute(query2, (name, street, city, state_ab, zip, bloodType, donorID))
+        mysql.connection.commit()
+        return redirect('/donors')
+        
 @app.route('/delete_donor/<int:donorID>', methods=['GET','POST'])
 def delete_donor(donorID):
     query_del = "DELETE FROM Donors WHERE donorID = '%s';"
-    cur_del = db.execute_query(db_connection=db_connection, query=query_del, query_params=(donorID,))
-    return render_template('success.j2')
+    cur = mysql.connection.cursor()
+    cur.execute(query_del, (donorID,))
+    mysql.connection.commit()
+    return redirect('/donors')
 
 # Listener
-
 if __name__ == "__main__":
-    #port = int(os.environ.get('PORT', 21037))
-    app.run(host='flip2.engr.oregonstate.edu', port=21037, debug=False) 
+    app.run(port=21039, debug=True)
